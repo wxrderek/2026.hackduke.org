7112da9bdb1ac5317de3acc1394c93a4b6b51380
Shuwen Wei <wshu07@gmail.com>
2025-11-23 14:00:24 -0500

    updated all static components for vending machine and left and right sides

--- src/sections/IntroSection.jsx
+++ src/sections/IntroSection.jsx
@@ -9,19 +9,18 @@ export default function IntroSection() {
         <img src="/images/left sign.svg" alt="" className="down1-left_sign"/>
         <div className="down1-left_side">
           <img src="/images/Lucky Cat.svg" alt="" className="down1-lucky_cat"/>
-          <img src="/images/entire left shelf.svg" alt="" className="down1-left_shelf"/>
+          <img src="/images/left shelf and assets.svg" alt="" className="down1-left_shelf"/>
+          <img src="/images/lower left.svg" alt="" className="down1-lower_left"/>
         </div>
         <img src="/images/right protrusion.svg" alt="" className="down1-right_protrusion"/>
         <div className="down1-right_side">
           <img src="/images/clay barrels.svg" alt="" className="down1-clay_barrels"/>
-          <img src="/images/entire right shelf.svg" alt="" className="down1-right_shelf"/>
+          <img src="/images/right shelf and assets.svg" alt="" className="down1-right_shelf"/>
           <img src="/images/right shelf underneath.svg" alt="" className="down1-right_shelf_underneath"/>
         </div>
         <div className="down1-vending">
-          <img src="/images/vending1.svg" alt="" className="down1-vending-image"/>
-          <img src="/images/Card Reader.svg" alt="" className="down1-vending-card_reader"/>
+          <img src="/images/vending.svg" alt="" className="down1-vending-image"/>
         </div>
-        {/* <img src="/images/lower right.svg" alt="" className="down1-lower_right"/> */}
       </section>
     </div>
   );
