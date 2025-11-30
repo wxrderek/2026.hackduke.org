d3f14cba11c716c40d258f7e37eda39fb6d773cd
Shuwen Wei <wshu07@gmail.com>
2025-11-30 16:34:55 -0500

    added can falling animation

--- src/sections/IntroSection.jsx
+++ src/sections/IntroSection.jsx
@@ -1,6 +1,30 @@
 import './IntroSection.css';
+import { useEffect, useRef, useState } from 'react';
 
 export default function IntroSection() {
+  const [canFall, setCanFall] = useState(false);
+  const canRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (canRef.current) {
+        const rect = canRef.current.getBoundingClientRect();
+        const triggerPoint = window.innerHeight * 1.0;
+
+        if (rect.top < triggerPoint && !canFall) {
+          setCanFall(true);
+        } else if (rect.top > triggerPoint + 100 && canFall) {
+          setCanFall(false);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [canFall]);
+
   return (
     <div className="intro-container">
       <section className="down1">
@@ -24,6 +48,12 @@ export default function IntroSection() {
           <img src="/images/vending.svg" alt="" className="down1-vending-image"/>
           <div className="down1-bottom_slot">
             <img src="/images/bottom slot lowest layer.svg" alt="" className="lowest_layer"/>
+            <img
+              ref={canRef}
+              src="/images/START POSITION.svg"
+              alt=""
+              className={`start-position ${canFall ? 'falling' : ''}`}
+            />
             <img src="/images/bottom slot top layer.svg" alt="" className="top_layer"/>
           </div>
         </div>
